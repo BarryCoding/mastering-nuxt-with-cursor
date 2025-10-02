1fa5685e145c81f813bb9964c48213bc16bcf353
Springer <zxllks@yahoo.com>
2025-10-02 10:16:09 +0800

    debug: server test

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,17 +1,25 @@
-import { createOpenAIModel, generateChatResponse } from '../services/ai-service'
+// import { createOpenAIModel, generateChatResponse } from '../services/ai-service'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const { messages } = body
   const id = messages.length.toString()
 
-  const openaiApiKey = useRuntimeConfig().openaiApiKey
-  const openaiModel = createOpenAIModel(openaiApiKey)
-  const content = await generateChatResponse(openaiModel, messages)
+  // const openaiApiKey = useRuntimeConfig().openaiApiKey
+  // const openaiModel = createOpenAIModel(openaiApiKey)
+  // const content = await generateChatResponse(openaiModel, messages)
+
+  // return {
+  //   id,
+  //   role: 'assistant',
+  //   content,
+  // }
+
+  const lastMessage = messages[messages.length - 1]
 
   return {
     id,
     role: 'assistant',
-    content,
+    content: `(server) You said: ${lastMessage.content}`,
   }
 })
